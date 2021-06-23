e295713d43afee32cd02dd8e5c448b9ad9051d38
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-06-24 02:43:28 +0530

    Update FileReaderin.java

--- FileReaderin.java
+++ FileReaderin.java
@@ -1,27 +1,25 @@
-package javab;
-
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
+package javab.Java_basic;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileReaderin {
 
-
 	public static void main(String[] args) {
-	/*	File file=new File("tmxt.txt");
+	File file=new File("tmxt.txt");
 		try {
 		FileReader reads;
 		BufferedReader read2=new BufferedReader(new FileReader(file));
-		String doc;
-		doc=read2.BufferedReader();
+		
+		String doc=read2.BufferedReader();
 		}catch(FileNotFoundException e) {
 			System.out.println(file.toString());
 		} catch(IOException e) {
 			System.out.println("unable to read");
 		}
-		*/
+		
 	}
 
 }
