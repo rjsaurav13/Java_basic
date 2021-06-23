46b5fe1384420964ced93314e439ccf1efda3ed6
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-06-24 02:43:37 +0530

    Create h.java

---
+++ h.java
@@ -0,0 +1,43 @@
+package javab.Java_basic;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class h {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					h window = new h();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public h() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
